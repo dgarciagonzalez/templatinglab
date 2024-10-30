8f2de3202755d2c2c6efa59b148aab6e8b60e1d1
denissegarcia <denisseg054@gmail.com>
2024-10-30 12:40:04 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+interface Ingredient{
+    void getName();
+    void getQuantity();
+
+}
+class SolidIngredient implements Ingredient{
+    private String _name;
+    private int _quantity;
+
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
